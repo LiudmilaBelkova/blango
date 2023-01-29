14702cbb948c8a07d4d6679f1513c0def2b6cc56
Lyudmila B <lbelkova@gmail.com>
2023-01-29 15:25:45 +0000

    Finish crispy forms

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -66,6 +66,15 @@ def row(extra_classes=""):
 def endrow():
     return format_html("</div>")
 
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")    
+
 
 # return 5 recent posts, excluding current post
 @register.inclusion_tag("blog/post-list.html")
